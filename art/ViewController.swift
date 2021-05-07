1de4348756bb00844a6eadbb9427a1a2ca97690f
itamar-marom <marom.itamar@gmail.com>
2021-05-07 11:59:58 +0300

    init

--- art/ViewController.swift
+++ art/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var header: UILabel!
+    @IBOutlet weak var headerButton: UIButton!
+    @IBAction func headerButton(_ sender: UIButton) {
+        header.text = "clicked"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        header.text = "hello"
+        
     }
-
-
+    
 }
 
